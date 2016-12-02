29a27a1b73340fa646de8ccac2d751160b953e40
Asachun <chuchuchu.chunchun@gmail.com>
2016-12-02 13:53:27 +0900

    Divide socket.

--- script/main.rb
+++ script/main.rb
@@ -4,19 +4,46 @@ server = TCPServer.open("127.0.0.1", 50000);
 
 puts "Server is running at 50000."
 
-socket = server.accept();
+parent = server.accept();
+puts("Parent come\n");
+parent.write("welcome\n");
 
-loop {
-  begin
-    message = socket.gets();
+child = server.accept();
+puts("Child come\n");
+
+parent.write("start\nparent\n");
+child.write("welcome\nstart\n");
+
+threads = Array.new();
+
+threads << Thread.new {
+  loop {
+    message = parent.gets();
+
+    if(message.nil?())
+      break;
+    end
+
+    puts("parent: #{message}");
+    parent.write(message);
+    child.write(message);
+  }
+}
+
+threads << Thread.new {
+  loop {
+    message = child.gets();
 
     if(message.nil?)
       break;
     end
 
-    puts(message);
-    socket.write(message);
-  rescue
-    break;
-  end
+    puts("child: #{message}");
+    parent.write(message);
+    child.write(message);
+  }
+}
+
+threads.each { |thread|
+  thread.join();
 }
