87382f8ffecf3aaec44a16deffda6ea57e6d04f6
Asachun <chuchuchu.chunchun@gmail.com>
2016-12-04 21:30:14 +0900

    Loop main process.

--- script/main.rb
+++ script/main.rb
@@ -4,59 +4,67 @@ server = TCPServer.open("127.0.0.1", 50000);
 
 puts "Server is running at 50000."
 
-parent = server.accept();
-puts("Parent come\n");
-parent.write("welcome\n");
+loop {
+  begin
+    parent = server.accept();
+    puts("Parent come\n");
+    parent.write("welcome\n");
 
-child = server.accept();
-puts("Child come\n");
+    child = server.accept();
+    puts("Child come\n");
 
-time = Time.now.to_i + 3;
+    time = Time.now.to_i + 3;
 
-parent.write("start,#{time}\nisParent\n");
-child.write("welcome\nstart,#{time}\n");
+    parent.write("start,#{time}\nisParent\n");
+    child.write("welcome\nstart,#{time}\n");
 
-threads = Array.new();
+    threads = Array.new();
 
-threads << Thread.new {
-  loop {
-    message = parent.gets();
+    threads << Thread.new {
+      loop {
+        message = parent.gets();
 
-    if (message.nil?())
-      break;
-    end
+        if (message.nil?())
+          break;
+        end
 
-    puts("parent: #{message}");
-    args = message.chop().split(",");
+        puts("parent: #{message}");
+        args = message.chop().split(",");
 
-    if (args[0] == "move")
-      parent.write("parent," + args[1] + "\n");
-      child.write("parent," + args[1] + "\n");
-    elsif (args[0] == "goal")
-      parent.write("goal\n");
-      child.write("goal\n");
-    end
-  }
-}
+        if (args[0] == "move")
+          parent.write("parent," + args[1] + "\n");
+          child.write("parent," + args[1] + "\n");
+        elsif (args[0] == "goal")
+          parent.write("goal\n");
+          child.write("goal\n");
+        end
+      }
+    }
 
-threads << Thread.new {
-  loop {
-    message = child.gets();
+    threads << Thread.new {
+      loop {
+        message = child.gets();
 
-    if (message.nil?())
-      break;
-    end
+        if (message.nil?())
+          break;
+        end
 
-    puts("child: #{message}");
-    args = message.split(",");
+        puts("child: #{message}");
+        args = message.split(",");
 
-    if (args[0] == "move")
-      parent.write("child," + args[1]);
-      child.write("child," + args[1]);
-    end
-  }
-}
+        if (args[0] == "move")
+          parent.write("child," + args[1]);
+          child.write("child," + args[1]);
+        end
+      }
+    }
 
-threads.each { |thread|
-  thread.join();
+    threads.each { |thread|
+      thread.join();
+    }
+  rescue
+    parent.close();
+    child.close();
+    puts("Game End");
+  end
 }
